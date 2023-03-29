f110694f251d28df870cc3d285635935fbdc37e7
Sagar <patilsagar1211@gmail.com>
2023-03-29 14:50:54 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  states:any;
+  searchTerm:any;
+  filteredstates:any;
+
+  constructor(private http:HttpClient){
+
+  }
+
+  ngOnInit(): void {
+    this.http.get("http://awsmaster.mahamining.com/master/states/GetState").subscribe((result:any)=>{
+      // console.log(result.responseData);
+      this.states = result.responseData;
+      this.filteredstates =  this.states;
+      
+    })
+  }
+
+  filter(){
+       this.filteredstates = this.states.filter((value:any, index:any)=>{
+        if(value.state.toLowerCase().indexOf(this.searchTerm.toLowerCase()) > -1){
+          return value
+        }
+       }) 
+  }
+
+}
